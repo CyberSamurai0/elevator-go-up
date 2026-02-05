85c99b135f7db5c3fff24e3a83d2037f91795111
Colin Saumure <36649127+CyberSamurai0@users.noreply.github.com>
2026-02-05 12:31:11 -0500

    Create buttons.c

---
+++ buttons.c
@@ -0,0 +1,31 @@
+#include "buttons.h"
+
+uint8_t getGPIOForFloor(uint8_t floor) {
+    switch (floor) {
+        case 1: return FLOOR_1_BUTTON_GPIO;
+        case 2: return FLOOR_2_BUTTON_GPIO;
+        case 3: return FLOOR_3_BUTTON_GPIO;
+        case 4: return FLOOR_4_BUTTON_GPIO;
+        case 5: return FLOOR_5_BUTTON_GPIO;
+        case 6: return FLOOR_6_BUTTON_GPIO;
+        case 7: return FLOOR_7_BUTTON_GPIO;
+        case 8: return FLOOR_8_BUTTON_GPIO;
+        case 9: return FLOOR_9_BUTTON_GPIO;
+        case 10: return FLOOR_10_BUTTON_GPIO;
+        case 11: return FLOOR_11_BUTTON_GPIO;
+        case 12: return FLOOR_12_BUTTON_GPIO;
+        case 13: return FLOOR_13_BUTTON_GPIO;
+        case 14: return FLOOR_14_BUTTON_GPIO;
+        case 15: return FLOOR_15_BUTTON_GPIO;
+        case 16: return FLOOR_16_BUTTON_GPIO;
+        default: return -1; // Invalid floor
+    }
+}
+
+void enableFloor(uint8_t floor) {
+    uint8_t gpio = getGPIOForFloor(floor);
+    if (gpio != -1) {
+        gpio_set_dir(gpio, GPIO_IN);
+        gpio_pull_up(gpio);
+    }
+}
