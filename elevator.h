967e75ba6b2770bec0dbcc16bc980bef5b1d56e5
Colin Saumure <36649127+CyberSamurai0@users.noreply.github.com>
2026-02-05 12:31:03 -0500

    Add extern to resolve multiple definition

--- elevator.h
+++ elevator.h
@@ -7,7 +7,7 @@
  */
 #include <stdint.h>
 
-uint8_t current_floor;
-uint8_t desired_floors[16];
-uint8_t direction; // 1 for up, -1 for down, 0 for idle
+extern uint8_t current_floor;
+extern uint8_t desired_floors[16];
+extern uint8_t direction; // 1 for up, -1 for down, 0 for idle
 
