514beb03a5412b6aa108004ac86078d5cf2011f1
Colin Saumure <36649127+CyberSamurai0@users.noreply.github.com>
2026-02-05 13:45:35 -0500

    Add ifndef block

--- elevator.h
+++ elevator.h
@@ -7,7 +7,11 @@
  */
 #include <stdint.h>
 
+#ifndef ELEVATOR_GO_UP_ELEVATOR_H
+#define ELEVATOR_GO_UP_ELEVATOR_H
+
 extern uint8_t current_floor;
 extern uint8_t desired_floors[16];
 extern uint8_t direction; // 1 for up, -1 for down, 0 for idle
 
+#endif //ELEVATOR_GO_UP_ELEVATOR_H
