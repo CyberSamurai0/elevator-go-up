98f8d255a00f626e313632286856e1614c9d674e
Colin Saumure <36649127+CyberSamurai0@users.noreply.github.com>
2026-02-05 08:44:35 -0500

    Update elevator.h

--- elevator.h
+++ elevator.h
@@ -1,3 +1,13 @@
+/**
+ * elevator.h
+ *
+ * Manages elevator state and operations.
+ * Author: Colin Saumure
+ *
+ */
 #include <stdint.h>
 
-uint8_t cur_floor;
+uint8_t current_floor;
+uint8_t desired_floors[16];
+uint8_t direction; // 1 for up, -1 for down, 0 for idle
+
